948d10e814e2873788b351ba6f72ad4cca43f3a3
Anne Hinrichs <annehinrichs22@gmail.com>
2017-12-14 21:54:57 +0100

    Update Les3:Sensor+ExtraTerminal-INO-Bestand.ino

--- Les3/Les3:Sensor+ExtraTerminal-INO-Bestand.ino
+++ Les3/Les3:Sensor+ExtraTerminal-INO-Bestand.ino
@@ -1,10 +1,9 @@
-int sensorPin = A0; // select the input pin for LDR
-int sensorValue = 0; // variable to store the value coming from the sensor
+int sensorValue = 0; // Dit maakt een variable met de naam sensorValue en de waarde 0, int betekent integer en is een geheel getal van 16 bits.
 void setup() {
-Serial.begin(9600); //sets serial port for communication
+Serial.begin(9600); // Opent een communicatiepoort met de computer zodat je data kunt sturen en ontvangen, 9600 is de snelheid, het is belangrijk dat je ontvangstsnelheid hetzelfde is.
 }
 void loop() {
-sensorValue = analogRead(sensorPin); // read the value from the sensor
-Serial.println(sensorValue); //prints the values coming from the sensor on the screen
-delay(100);
+sensorValue = analogRead(A0); // Leest de waarde van pin A0 en stopt die in de variabele met de naam sensorValue
+Serial.println(sensorValue); // Print de waarde van sensorValue via de seriële monitor (de communicatiepoort) op het scherm van je computer.
+delay(100); // Wacht 100 miliseconden, 0,1 sec
 }
