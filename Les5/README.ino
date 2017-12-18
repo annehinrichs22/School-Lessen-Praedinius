455fc6627291229975e0706c3f79b62046a2ea58
Anne Hinrichs <annehinrichs22@gmail.com>
2017-12-18 21:21:05 +0100

    Create README.ino

---
+++ Les5/README.ino
@@ -0,0 +1,8 @@
+### Les 5: De LDR data omzetten in de knippersnelheid.
+In les 1 kon je door de delays tussen het aan- en uitzetten van de LED de knippersnelheid aanpassen. In deze les gaan we de data van de LDR gebruiken om de delay aan te passen.
+
+### Stap 1:
+Je kunt de LED en de LDR aansluiten zoals in les 1 en 2.
+
+### Stap 2:
+Moeilijke route: de delay van de LED is nog leuk tot ongeveer 500 miliseconden, daarna is hij te lang vind ik. De waarde van de LDR zit altijd tussen de 0 en 1024. Voor deze opdracht kun je in de Arduino taal gebruik maken van de Map functie. De map functie werkt zo:
